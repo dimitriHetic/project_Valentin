12376fb3ee565e2f4bf476ce591cc6e7cb373e01
Valentin Rabin <valentin.rabin@epita.fr>
2021-01-20 13:32:05 +0100

    add button

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View} from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 import {circle} from './components/circle';
 import {cross} from './components/cross';
 import {grid} from './components/grid';
@@ -8,6 +8,12 @@ import {grid} from './components/grid';
 const Circle = circle
 const Cross = cross
 const Grid = grid
+
+function truc(lautre){
+  lautre.backgroundColor = 'red';
+  console.log("truc");
+}
+
 export default function App() {
   return (
     <View style={styles.container}>
@@ -15,6 +21,61 @@ export default function App() {
       <Cross />
       <Grid/>
       <StatusBar style="auto" />
+
+      <TouchableOpacity style = {styles.bouton1}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton2}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton3}>
+        
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton4}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton5}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton6}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton7}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton8}>
+      <Text >
+         Butto
+      </Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style = {styles.bouton9}>
+      <Text >
+         Button
+      </Text>
+      </TouchableOpacity>
       
     </View>
     
@@ -27,5 +88,77 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  bouton1:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 500,
+    left: 50,
+    backgroundColor: 'green',
+  },
+  bouton2:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 500,
+    left: 110,
+    backgroundColor: 'green',
+  },
+  bouton3:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 500,
+    left: 170,
+    backgroundColor: 'green',
+  },
+  bouton4:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 440,
+    left: 50,
+    backgroundColor: 'green',
+  },
+  bouton5:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 440,
+    left: 110,
+    backgroundColor: 'green',
+  },
+  bouton6:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 440,
+    left: 170,
+    backgroundColor: 'green',
+  },
+  bouton7:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 380,
+    left: 50,
+    backgroundColor: 'green',
+  },
+  bouton8:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 380,
+    left: 110,
+    backgroundColor: 'green',
+  },
+  bouton9:{
+    height: 50,
+    width: 50,
+    position: 'absolute',
+    bottom: 380,
+    left: 170,
+    backgroundColor: 'green',
   },
 });
