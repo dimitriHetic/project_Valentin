d7dad0a827ec459e672423df39f8213abf32cb8a
Valentin Rabin <valentin.rabin@epita.fr>
2021-02-01 16:19:47 +0100

    Fonctionnelle gagant dans le log

--- App.js
+++ App.js
@@ -10,27 +10,145 @@ let Circle = circle
 const Cross = cross
 const Grid = grid
 
+function verifWin(playArray)
+{
+  if(playArray[0] == playArray[1] && playArray[0]  == playArray[2] && playArray[0]!= null){
+    return playArray[0]
+  }
+  
+  if(playArray[3] == playArray[4] && playArray[3] == playArray[5] && playArray[3]!= null){
+    return playArray[3]
+  }
+
+  if(playArray[6] == playArray[7] && playArray[6] == playArray[8] && playArray[6]!= null){
+    return playArray[6]
+  }
+
+  if(playArray[0] == playArray[3] && playArray[0] == playArray[6] && playArray[0]!= null){
+    return playArray[0]
+  }
+
+  if(playArray[1] == playArray[4] && playArray[1] == playArray[7] && playArray[1]!= null){
+    return playArray[1]
+  }
+
+  if(playArray[2] == playArray[5] && playArray[2] == playArray[8] && playArray[2]!= null){
+    return playArray[2]
+  }
+
+  if(playArray[0] == playArray[4] && playArray[0] == playArray[8] && playArray[0]!= null){
+    return playArray[0]
+  }
+
+  if(playArray[2] == playArray[4] && playArray[2] == playArray[6] && playArray[2]!= null){
+    return playArray[2]
+  }
+}
 
 
 export default class App extends React.Component {
 
-  constructor(props){
+  
+
+  constructor(props)
+  {
     super(props)
-    this.state = {left: 0}
-      }
+    this.state = { GridArray: []}
+    this.index = 0;
+    this.color = 'red'
+    this.playArray = new Array(9)
+    this.winner = 0
+  }
+
+  
   
   
   _truc(i){
-    console.log("appuyer"+i)
-    this.setState({left: i*100})
+
+
+    if(i == 1 && this.playArray[0] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {300} bot = {550} color = {this.color}/>})});
+    }
+    if(i == 2 && this.playArray[1] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {560} bot = {550} color = {this.color}/>})});
+    }
+    if(i == 3 && this.playArray[2] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {820} bot = {550} color = {this.color}/>})});
+    }
+    if(i == 4 && this.playArray[3] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {300} bot = {300} color = {this.color}/>})});
+    }
+    if(i == 5 && this.playArray[4] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {560} bot = {300} color = {this.color}/>})});
+    }
+    if(i == 6 && this.playArray[5] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {820} bot = {300} color = {this.color}/>})});
+    }
+    if(i == 7 && this.playArray[6] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {300} bot = {50} color = {this.color}/>})});
+    }
+    if(i == 8 && this.playArray[7] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {560} bot = {50} color = {this.color}/>})});
+    }
+    if(i == 9 && this.playArray[8] == null)
+    {
+      this.setState({GridArray: this.state.GridArray.concat({key: this.index,form: <Circle left = {820} bot = {50} color = {this.color}/>})});
+    }
+
+    if(this.color == 'red')
+    {
+      this.playArray[i-1] = 1 
+    }
+    else
+    {
+      this.playArray[i-1] = 2
+    }
+
+    this.index += 1
+
+    if(this.color == 'red')
+    {
+      this.color = 'blue'
+    }
+    else
+    {
+      this.color = 'red'
+    }
+
+    //console.log(this.playArray[0],this.playArray[1],this.playArray[2],'\n',this.playArray[3],this.playArray[4],this.playArray[5],'\n',this.playArray[6],this.playArray[7],this.playArray[8])
+
+    this.winner = verifWin(this.playArray)
+
+    if(this.winner != null)
+    {
+      console.log(this.winner)
+    }
   }
 render(){
+
+  const renderList = this.state.GridArray.map((data) => {
+
+      return(
+        data.form
+      )
+
+  })
+
   return (
     <View style={styles.container}>
-      <Circle left = {this.state.left} />
       <Cross />
       <Grid/>
       <StatusBar style="auto" />
+
+      {renderList}
 
       <TouchableOpacity style = {styles.bouton1} onPress = {()=>this._truc(1)}>
       <Text >
--- components/circle.js
+++ components/circle.js
@@ -6,7 +6,7 @@ class circle extends React.Component{
 render(){
     return( 
         
-        <View style={{height: 150, width:150, borderRadius:100, backgroundColor: 'red', position: 'absolute', bottom: 500,left: this.props.left,zIndex: 1000}}></View>
+        <View style={{height: 150, width:150, borderRadius:100, backgroundColor: this.props.color, position: 'absolute', bottom: this.props.bot,left: this.props.left,zIndex: 1000}}></View>
         )
     }
 }
